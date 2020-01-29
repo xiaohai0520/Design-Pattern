13760fa9039b8082b953380a3cd3a098aa21eb2a
XiaohaiSun <37384330+xiaohai0520@users.noreply.github.com>
2020-01-28 22:37:37 -0500

    Add files via upload

---
+++ 02 - decorator pattern/Beverage.java
@@ -0,0 +1,13 @@
+public abstract class Beverage {
+
+
+    String description = "Unknow Beverage";
+
+    public String getDescription(){
+
+        return description;
+    }
+
+    public abstract double cost();
+
+}
---
+++ 02 - decorator pattern/CoffeeBar.java
@@ -0,0 +1,13 @@
+public class CoffeeBar {
+
+    public static void main(String args[]){
+
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " $ " + beverage.cost());
+
+        beverage = new Mocha(beverage);
+        beverage = new Mocha(beverage);
+        System.out.println(beverage.getDescription() + " $ " + beverage.cost());
+
+    }
+}
